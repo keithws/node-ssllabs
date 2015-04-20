a095fafc49d0c2de3ce6c31f9af325405dda1aed
Keith Shaw <github@keithws.net>
2015-04-20 03:52:58 -0700

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # node-ssllabs
-A node.js module for the SSL Labs API.
+
+A node.js library for the [SSL Labs API][1].
+
+## Install
+
+	npm install node-ssllabs
+
+## Usage
+
+	var ssllabs = require("ssllabs");
+	
+	ssllabs.analyze({ "host": "ssllabs.com" }, function (err, host) {
+		console.dir(host);
+	});
+
+## Requirements
+
+underscore.js
+
+## Dev Requirements
+
+mocha.js
+should.js
+
+## Change Log
+
+0.0.1 — April 20, 2015
+
+* Initial version
+* supports two api end-points, `info` and `analyze`
+
+
+  [1]: https://www.ssllabs.com/projects/ssllabs-apis/
---
+++ test/ssllabs.js
@@ -0,0 +1,67 @@
+var should = require("should"),
+mocha = require("mocha"),
+ssllabs = require("../lib/ssllabs.js");
+
+describe("ssllabs", function () {
+
+	describe("library", function () {
+
+		it("should check SSL Labs availability", function (done) {
+			ssllabs.info(function (err, info) {
+				if (err) {
+					return done(err);
+				}
+				info.should.have.properties([
+					"criteriaVersion",
+					"currentAssessments",
+					"engineVersion",
+					"maxAssessments",
+					"messages"
+				]);
+				info.currentAssessments.should.be.a.number;
+				info.maxAssessments.should.be.a.number;
+				info.messages.should.be.an.array;
+				done();
+			});
+		});
+
+		it("should invoke assessment", function(done) {
+			var options;
+
+			options = {
+				all: "done",
+				host: "keithws.net",
+				publish: true,
+				startNew: false
+			};
+
+			ssllabs.analyze(options, function (err, host) {
+				if (err) {
+					return done(err);
+				}
+				host.status.should.be.ok;
+				done();
+			});
+		});
+
+		it("should check progress", function(done) {
+			var options;
+
+			options = {
+				all: "done",
+				host: "keithws.net",
+				publish: true
+			};
+
+			ssllabs.analyze(options, function (err, host) {
+				if (err) {
+					return done(err);
+				}
+				host.status.should.be.ok;
+				done();
+			});
+		});
+
+	});
+
+});
